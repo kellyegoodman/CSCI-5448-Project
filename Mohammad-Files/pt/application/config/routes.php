6576d36c7d1bec658ea4e20f8bb51f75b45facbd
Kevin Holligan <kholligan@gmail.com>
2015-12-02 23:26:29 -0700

    Added login, logout, register to project

--- Mohammad-Files/pt/application/config/routes.php
+++ Mohammad-Files/pt/application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'projects_controller/index';
+$route['default_controller'] = 'login_controller';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 //$route['(:any)'] = 'pages/view/$1';
